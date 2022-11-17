cdec3de7201733a2a251e9111c10b693290c0e40
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-17 09:48:39 +0300

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+const convertToPoints = (lngLat) => {
+    return {
+      point: {
+        latitude: lngLat.lat,
+        longitude: lngLat.lng
+      }
+    }
+  }
+
+
+   const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+      const callParameters = {
+        key: process.env.REACT_APP_TOM_TOM_API_KEY,
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      }
